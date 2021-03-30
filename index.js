a8b0e47d99c7d0b7eae359835c8503f0bce89259
William David Martinez Perez <wdmartinez89@gmail.com>
2021-03-30 12:37:40 -0500

    Added another condition

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 // it allows the user to give a word and the program will change it to an alert
 let word = prompt('Give me a word');
 // now i want a condition that says: the alert will only occur if the word has 6 characters or less
-if (word.length < 7) {
+if (word.length === 0) {
+    alert('You need to type the word you want to change')
+} else if (word.length < 7) {
     word = word + '!';
     word = word.toUpperCase();
     alert(word);
 } else {
     alert('Your word has more than 6 characters')
 }
+
+
+    
