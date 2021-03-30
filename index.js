cf9ba8eccb423e30026790ec80870bc008bb5130
William David Martinez Perez <wdmartinez89@gmail.com>
2021-03-30 12:25:32 -0500

    Removed not needed lines of code

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // My first program 
 // it allows the user to give a word and the program will change it to an alert
 let word = prompt('Give me a word');
-// word = word + '!';
-// word = word.toUpperCase();
-// alert(word);
 // now i want a condition that says: the alert will only occur if the word has 6 characters or less
 if (word.length < 7) {
     word = word + '!';
