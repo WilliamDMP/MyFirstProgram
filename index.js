f0f6518e8f7ba92b632f535b9b8d89aaa3d41c96
William David Martinez Perez <wdmartinez89@gmail.com>
2021-03-30 11:38:10 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // My first program 
-// it allows the user to give a word to change it as an alert
+// it allows the user to give a word and the program will change it to an alert
+let word = prompt('Give me a word');
+word = word + '!';
+word = word.toUpperCase();
+alert(word);
