d4b017d3cfada541d1dd1f10656d73ab79d27f70
William David Martinez Perez <wdmartinez89@gmail.com>
2021-03-30 12:22:46 -0500

    Added a conditional

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // My first program 
 // it allows the user to give a word and the program will change it to an alert
 let word = prompt('Give me a word');
-word = word + '!';
-word = word.toUpperCase();
-alert(word);
+// word = word + '!';
+// word = word.toUpperCase();
+// alert(word);
+// now i want a condition that says: the alert will only occur if the word has 6 characters or less
+if (word.length < 7) {
+    word = word + '!';
+    word = word.toUpperCase();
+    alert(word);
+} else {
+    alert('Your word has more than 6 characters')
+}
